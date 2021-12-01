aad6e0e937398e63cc3822eb7893cf48493f162c
Jan Świątek <jan.swiatek@protonmail.com>
2021-12-01 02:23:17 +0100

    OOV

--- main.py
+++ main.py
@@ -1,80 +1,83 @@
 import cv2
+import numpy
+
 from colorsys import hsv_to_rgb
-import numpy as np
 
 
-def contrast_correction(img):
-    equ = cv2.equalizeHist(img)
+class Image:
+    def __init__(self, _raw):
+        self.raw = _raw
 
-    return np.hstack((img, equ))  # stacking images side-by-side
+    # @property
+    # def width(self):
+    #     return self.raw.shape[0]
+    #
+    # @property
+    # def height(self):
+    #     return self.raw.shape[1]
+
+    @property
+    def preprocessed(self):
+        img = cv2.cvtColor(self.raw, cv2.COLOR_BGR2GRAY)
+        img = cv2.bilateralFilter(img, 25, 75, 75)
+        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        img = cv2.bitwise_not(img)
+
+        return img
 
 
-def gamma_correction(img, gamma):
-    table = np.empty((1, 256), np.uint8)
+class Frame(Image):
+    @property
+    def boards(self):
+        contours, _ = cv2.findContours(self.preprocessed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    for i in range(256):
-        table[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+        for j, contour in enumerate(contours):
+            a = numpy.min(contour[:, :, 1])
+            b = numpy.max(contour[:, :, 1])
+            c = numpy.min(contour[:, :, 0])
+            d = numpy.max(contour[:, :, 0])
+        #
+        #     print(a, b, c, d)
+        #     img_tmp = self.raw[a:b, c:d]
+        #     print(img_tmp)
+            cv2.imshow("Display", self.raw[a:b, c:d])
 
-    return cv2.LUT(img, table)
+            # cv2.drawContours(self.raw, [contour], -1, numpy.array(hsv_to_rgb(1 / len(contours) * j, 1, 1)) * 255.0, 5)
+            # cv2.imshow("Boards", self.raw)
+
+            # potential_boards.append(board)
+
+        # for potential_board in potential_boards:
+        #     potential_board.display()
+        # return contours
+
+
+class Board(Image):
+    pass
 
 
 def main(url):
     while True:
-        response, img = cv2.VideoCapture(url).read()
+        success, raw = cv2.VideoCapture(url).read()
 
-        if img is not None:
-            cv2.imshow("Tic tac toe", img)
-
-        key = cv2.waitKey(1)
-
-        if key == 27:           # ESC
+        if not success:
             break
-        elif key == 13:         # Enter
-            processing(img)
+
+        frame = Frame(raw)
+        key   = cv2.waitKey(1)
+
+        cv2.imshow("Tic tac toe", frame.raw)
+
+        if key == 27:
+            break
+        elif key == 13:
+            cv2.imshow("Frame", frame.preprocessed)
 
     cv2.destroyAllWindows()
 
 
-def processing(img):
-    # Initial game state
-    state = [
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-        ["-", "-", "-"]
-    ]
-
-    # kernel used for noise removal
-    kernel = np.ones((7, 7), np.uint8)
-
-    # get the image width and height
-    img_width = img.shape[0]
-    img_height = img.shape[1]
-
-    # turn into grayscale
-    img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # img_b = cv2.medianBlur(img_g, 21)
-
-    # turn into thresholded binary
-    ret, thresh1 = cv2.threshold(img_g, 40, 255, cv2.THRESH_BINARY)
-
-    # remove noise from binary
-    thresh1 = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
-
-    img_inv = cv2.bitwise_not(thresh1)
-
-    # find and draw contours. RETR_EXTERNAL retrieves only the extreme outer contours
-    contours, hierarchy = cv2.findContours(img_inv, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 5)
-    for i, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        cv2.drawContours(img, [contour], -1, np.array(hsv_to_rgb(i / len(contours), 1, 1)) * 255.0, 5)
-
-    cv2.imshow("Processing", img)
-
-
-
 if __name__ == "__main__":
-    url = 'http://192.168.1.66:8080/video'
-    main(url)
+    address = "192.168.1.66"
+    port    = "8080"
 
+    main(f"https://{address}:{port}/video")
